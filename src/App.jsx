afb403846ab05c943cd10857f628c3efe84abe4b
Paula <paulajimenezt20@gmail.com>
2024-03-09 17:43:24 -0500

    added file structure and mui

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Album from "components/album/album";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Album />
+    </div>
+  );
+}
+
+export default App;
